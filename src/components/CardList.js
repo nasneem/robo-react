956e18d7a39676d98df517f790a4ed3a3583279b
nasneem <nasneem.cse@gmail.com>
2019-08-04 09:03:56 +0530

    first commit

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Card from './Card';
 const CardList = ({ robots })=>{	
-	if(true){
-		throw new Error('Noooo error created in cardlist!');
-	}
+	// if(true){
+	// 	throw new Error('Noooo error created in cardlist!');
+	// }
 	const cardComponent = robots.map((user,i) => {
 		return <Card key ={i} id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
 	})
